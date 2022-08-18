7c74d1a2d089413a7faeafe0dad45162d2d3d4c9
TailonNecke <82898005+TailonNecke@users.noreply.github.com>
2022-08-18 10:27:41 -0300

    configuração de sequence para auto increment

--- src/app/services/ListarMensagens.ts
+++ src/app/services/ListarMensagens.ts
@@ -8,7 +8,7 @@ export async function listarMensagens(){
     const Monitor = conn.getRepository(Modele_Exemplo);
 
     try{
-    const mensagens = await Monitor.createQueryBuilder().select().getRawMany()
+    const mensagens = await Monitor.createQueryBuilder().select().orderBy("Modelo_Exemplo.modelo_id").getRawMany()
     if(!mensagens[0]){
         return {Error: "Sem mensagens cadastradas"}
     }
--- src/database/migrations/Mon/1658420051089-ModeloExemplo.ts
+++ src/database/migrations/Mon/1658420051089-ModeloExemplo.ts
@@ -1,34 +1,35 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm"
-import { query } from "express"
+    import { MigrationInterface, QueryRunner, Table,  } from "typeorm"
 
-export class ModeloExemplo1658420051089 implements MigrationInterface {
+    export class ModeloExemplo1658420051089 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    
-        await queryRunner.createTable(new Table({
-            name: "Modelo_Exemplo",
-            columns: [
-                {
-                    name: "modelo_id",
-                    type: "number",
-                    isPrimary: true,
-                    isGenerated: true,
-                    generationStrategy: 'increment',
-                },
-                {
-                    name: "nome",
-                    type: "varchar",
-                },
-                {
-                    name: "mensagem",
-                    type: "varchar",
-                },
-            ],
-        }));
+        public async up(queryRunner: QueryRunner): Promise<void> {
+
+            // !!!!!! Nome da sequencia tem que ser em maiusculo!!!!! 
+            await queryRunner.query(`CREATE SEQUENCE MODELO_EXEMPLO INCREMENT BY 1 START WITH 1 MINVALUE 1 MAXVALUE 99999999999999999999 NOCACHE`); 
+            await queryRunner.createTable(new Table({
+                name: "Modelo_Exemplo",
+                columns: [
+                    {
+                        name: "modelo_id",
+                        type: "varchar",
+                        isPrimary: true,
+                        default: '"SEQ_WMO"."MODELO_EXEMPLO".nextval',
+                    },
+                    {
+                        name: "nome",
+                        type: "varchar",
+                    },
+                    {
+                        name: "mensagem",
+                        type: "varchar",
+                    },
+                ],
+            }));
+        }
+
+        public async down(queryRunner: QueryRunner): Promise<void> {
+            await queryRunner.dropTable( 'Modelo_Exemplo');
+            await queryRunner.query( 'DROP SEQUENCE "MODELO_EXEMPLO"');
+        }
+
     }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable( 'Modelo_Exemplo');
-    }
-
-}
